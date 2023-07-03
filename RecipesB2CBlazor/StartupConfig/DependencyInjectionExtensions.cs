7c57898f321d372e97652da22b090fc32d4bb389
Tyler Barlow <barlowtyler96@gmail.com>
2023-07-03 13:54:56 -0400

    removed unused usings

--- RecipesB2CBlazor/StartupConfig/DependencyInjectionExtensions.cs
+++ RecipesB2CBlazor/StartupConfig/DependencyInjectionExtensions.cs
@@ -1,5 +1,9 @@
-﻿using RecipesB2CBlazor.Services;
-using System.Runtime.CompilerServices;
+﻿using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc.Authorization;
+using Microsoft.Identity.Web;
+using Microsoft.Identity.Web.UI;
+using RecipesB2CBlazor.Services;
 
 namespace RecipesB2CBlazor.StartupConfig;
 
@@ -12,6 +16,18 @@ public static class DependencyInjectionExtensions
 
     public static void AddAuthServices(this WebApplicationBuilder builder)
     {
-        
+        builder.Services.AddMicrosoftIdentityWebAppAuthentication(builder.Configuration, "AzureAdB2C")
+            .EnableTokenAcquisitionToCallDownstreamApi(new string[] { builder.Configuration["DownstreamApi:Scopes"] })
+            .AddInMemoryTokenCaches();
+
+        builder.Services.AddControllersWithViews(options =>
+        {
+            var policy = new AuthorizationPolicyBuilder()
+                .RequireAuthenticatedUser()
+                .Build();
+            options.Filters.Add(new AuthorizeFilter(policy));
+        }).AddMicrosoftIdentityUI();
+
+        builder.Services.Configure<OpenIdConnectOptions>(builder.Configuration.GetSection("AzureAdB2C"));
     }
 }
