38f5f277c17dcacef438d09628f366107ab51069
Tyler Barlow <barlowtyler96@gmail.com>
2023-07-19 17:23:11 -0400

    removed unused cookies config

--- RecipesB2CBlazor/Program.cs
+++ RecipesB2CBlazor/Program.cs
@@ -3,15 +3,6 @@ using RecipesB2CBlazor.StartupConfig;
 
 var builder = WebApplication.CreateBuilder(args);
 
-//builder.Services.AddDistributedMemoryCache();
-
-//builder.Services.Configure<CookiePolicyOptions>(opts =>
-//{
-//    opts.CheckConsentNeeded = context => true;
-//    opts.MinimumSameSitePolicy = SameSiteMode.Unspecified;
-//    opts.HandleSameSiteCookieCompatibility();
-//});
-
 builder.AddCustomServices();
 builder.AddAuthServices();
 
@@ -31,7 +22,6 @@ if (!app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseStaticFiles();
-app.UseCookiePolicy();
 
 app.UseRouting();
 
