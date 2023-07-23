16d8ceb07f7a2a3ef1cd2d6714435b14a4315c1c
Tyler Barlow <barlowtyler96@gmail.com>
2023-07-23 16:48:30 -0400

    updated auth services for multiple auth flows

--- RecipesB2CBlazor/StartupConfig/DependencyInjectionExtensions.cs
+++ RecipesB2CBlazor/StartupConfig/DependencyInjectionExtensions.cs
@@ -1,10 +1,8 @@
-﻿using Microsoft.AspNetCore.Authentication.OpenIdConnect;
-using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc.Authorization;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
 using Microsoft.Identity.Web;
 using Microsoft.Identity.Web.UI;
 using RecipesB2CBlazor.Services;
-
 namespace RecipesB2CBlazor.StartupConfig;
 
 public static class DependencyInjectionExtensions
@@ -16,19 +14,16 @@ public static class DependencyInjectionExtensions
 
     public static void AddAuthServices(this WebApplicationBuilder builder)
     {
-
-        builder.Services.AddAuthentication()
-            .AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureAd"))
+        builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureAd"), JwtBearerDefaults.AuthenticationScheme, null)
             .EnableTokenAcquisitionToCallDownstreamApi(new string[] { builder.Configuration["DownstreamApi:Scopes"] })
             .AddInMemoryTokenCaches();
 
+        builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+            .AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureAdB2C"), OpenIdConnectDefaults.AuthenticationScheme);
+
+        builder.Services.AddHttpContextAccessor();
+
         builder.Services.AddControllersWithViews().AddMicrosoftIdentityUI();
-
-        //builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
-        //    .AddMicrosoftIdentityWebApp(builder.Configuration.GetSection("AzureAdB2C"))
-        //    .EnableTokenAcquisitionToCallDownstreamApi(new string[] { builder.Configuration["DownstreamApi:Scopes"] })
-        //    .AddInMemoryTokenCaches();
-
-        //builder.Services.AddControllersWithViews().AddMicrosoftIdentityUI();
     }
 }
