25afbf47c32a71dd62c2bc7977b4df6f7e0de578
Tyler Barlow <barlowtyler96@gmail.com>
2023-08-01 20:53:40 -0400

    added pagination response model for recipes

---
+++ RecipesB2CBlazor/Helpers/RecipesResponse.cs
@@ -0,0 +1,14 @@
+﻿using RecipesB2CBlazor.Models;
+
+namespace RecipesB2CBlazor.Helpers;
+
+public class RecipesResponse
+{
+    public int TotalCount { get; set; }
+
+    public int PageSize { get; set; } = 6;
+
+    public int CurrentPageNumber { get; set; } = 1;
+
+    public List<RecipeModel> Data { get; set; }
+}
