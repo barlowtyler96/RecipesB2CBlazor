5fc7a5879e3f89b361493f54c8b9a554e4a4b52c
Tyler Barlow <barlowtyler96@gmail.com>
2023-08-26 16:59:53 -0400

    added data annotations for form

--- RecipesB2CBlazor/Models/RecipeDto.cs
+++ RecipesB2CBlazor/Models/RecipeDto.cs
@@ -1,16 +1,22 @@
-﻿namespace RecipesB2CBlazor.Models;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RecipesB2CBlazor.Models;
 
 public class RecipeDto
 {
     public int Id { get; set; }
 
+    [Required]
     public string? Name { get; set; }
 
+    [Required]
     public string? Description { get; set; }
 
+    [Required]
     public string? Instructions { get; set; }
 
     public string? ImagePath { get; set; }
 
+    [Required]
     public List<RecipeIngredient> RecipeIngredients { get; set; } = new List<RecipeIngredient>();
 }
