55e3e988583c434afa0e230f22c1053aa809fca1
Tyler Barlow <barlowtyler96@gmail.com>
2023-08-27 19:17:15 -0400

    naming

--- RecipesB2CBlazor/Models/RecipeDto.cs
+++ RecipesB2CBlazor/Models/RecipeDto.cs
@@ -1,22 +1,24 @@
-﻿using System.ComponentModel.DataAnnotations;
-
+﻿using Newtonsoft.Json;
+using System.ComponentModel.DataAnnotations;
 namespace RecipesB2CBlazor.Models;
 
 public class RecipeDto
 {
-    public int Id { get; set; }
+    public int RecipeId { get; set; } 
 
     [Required]
+    [MaxLength(50)]
     public string? Name { get; set; }
 
     [Required]
+    [MaxLength(2000)]
     public string? Description { get; set; }
 
     [Required]
+    [MaxLength(1000)]
     public string? Instructions { get; set; }
 
+    [MaxLength(200)]
     public string? ImagePath { get; set; }
-
-    [Required]
-    public List<RecipeIngredient> RecipeIngredients { get; set; } = new List<RecipeIngredient>();
 }
+
--- RecipesB2CBlazor/Models/RecipeModel.cs
+++ RecipesB2CBlazor/Models/RecipeModel.cs
@@ -1,28 +1,22 @@
-﻿using Newtonsoft.Json;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
+
 namespace RecipesB2CBlazor.Models;
 
-public class RecipeModel
+public class RecipeModel    
 {
-    public int Id { get; set; }
+    public int RecipeId { get; set; }
 
     [Required]
-    [MaxLength(50)]
     public string? Name { get; set; }
 
     [Required]
-    [MaxLength(2000)]
     public string? Description { get; set; }
 
     [Required]
-    [MaxLength(2000)]
-    public string? Ingredients { get; set; }
-
-    [Required]
-    [MaxLength(1000)]
     public string? Instructions { get; set; }
 
-    [MaxLength(200)]
     public string? ImagePath { get; set; }
-}
 
+    [Required]
+    public List<RecipeIngredient> RecipeIngredients { get; set; } = new List<RecipeIngredient>();
+}
