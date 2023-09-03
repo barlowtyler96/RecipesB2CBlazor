7486a22811bad759c8d8b15c5510632d29a95ce7
Tyler Barlow <barlowtyler96@gmail.com>
2023-09-03 18:01:04 -0400

    added model for file uploads

---
+++ RecipesB2CBlazor/Models/FileUploadViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace RecipesB2CBlazor.Models;
+
+public class FileUploadViewModel
+{
+    public string? FileName { get; set; }
+    public string? FileStorageUrl { get; set; }
+    public string? ContentType { get; set; }
+}
