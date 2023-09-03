600a5e5d43586a6f034f0412117c0833a7060041
Tyler Barlow <barlowtyler96@gmail.com>
2023-09-03 18:01:39 -0400

    added blob storage services

---
+++ RecipesB2CBlazor/Services/BlobStorageService.cs
@@ -0,0 +1,51 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+namespace RecipesB2CBlazor.Services;
+public class BlobStorageService : IBlobStorageService
+{
+    private readonly IConfiguration _configuration;
+    string blobStorageconnection = string.Empty;
+    private string blobContainerName = "recipevaultimages";
+    public BlobStorageService(IConfiguration configuration)
+    {
+        _configuration = configuration;
+        blobStorageconnection = _configuration.GetConnectionString("AzureStorageAccount");
+    }
+    public async Task<string> UploadFileToBlobAsync(string strFileName, string contentType, Stream fileStream)
+    {
+        try
+        {
+            var container = new BlobContainerClient(blobStorageconnection, blobContainerName);
+            var createResponse = await container.CreateIfNotExistsAsync();
+            if (createResponse != null && createResponse.GetRawResponse().Status == 201)
+                await container.SetAccessPolicyAsync(Azure.Storage.Blobs.Models.PublicAccessType.Blob);
+            var blob = container.GetBlobClient(strFileName);
+            await blob.DeleteIfExistsAsync(DeleteSnapshotsOption.IncludeSnapshots);
+            await blob.UploadAsync(fileStream, new BlobHttpHeaders { ContentType = contentType });
+            var urlString = blob.Uri.ToString();
+            return urlString;
+        }
+        catch (Exception ex)
+        {
+            throw;
+        }
+    }
+
+    public async Task<bool> DeleteFileToBlobAsync(string strFileName)
+    {
+        try
+        {
+            var container = new BlobContainerClient(blobStorageconnection, blobContainerName);
+            var createResponse = await container.CreateIfNotExistsAsync();
+            if (createResponse != null && createResponse.GetRawResponse().Status == 201)
+                await container.SetAccessPolicyAsync(Azure.Storage.Blobs.Models.PublicAccessType.Blob);
+            var blob = container.GetBlobClient(strFileName);
+            await blob.DeleteIfExistsAsync(DeleteSnapshotsOption.IncludeSnapshots);
+            return true;
+        }
+        catch (Exception ex)
+        {
+            throw;
+        }
+    }
+}
---
+++ RecipesB2CBlazor/Services/IBlobStorageService.cs
@@ -0,0 +1,6 @@
+﻿namespace RecipesB2CBlazor.Services;
+public interface IBlobStorageService
+{
+    Task<bool> DeleteFileToBlobAsync(string strFileName);
+    Task<string> UploadFileToBlobAsync(string strFileName, string contecntType, Stream fileStream);
+}
