2c918ed28d8c1844d43e3422d9aa68e854e5939b
Tyler Barlow <barlowtyler96@gmail.com>
2023-09-05 19:44:54 -0400

    changed return type to int so i can display created recipe by id

--- RecipesB2CBlazor/Services/IRecipesService.cs
+++ RecipesB2CBlazor/Services/IRecipesService.cs
@@ -1,16 +1,15 @@
 ﻿using RecipesB2CBlazor.Helpers;
 using RecipesB2CBlazor.Models;
 
-namespace RecipesB2CBlazor.Services
+namespace RecipesB2CBlazor.Services;
+
+public interface IRecipesService
 {
-    public interface IRecipesService
-    {
-        Task<bool> AddAsync(RecipeModel recipeDto);
-        Task DeleteAsync(int id);
-        Task<RecipeDto> EditAsync(RecipeDto recipeModel);
-        Task<List<RecipeDto>> GetAllAsync();
-        Task<RecipeModel> GetAsync(int id);
-        Task<RecipesResponse> GetByKeyword(string keyword, int currentPageNumber, int pageSize);
-        Task<List<RecipeDto>> GetRecentsAsync();
-    }
+    Task<int> AddAsync(RecipeModel recipeDto);
+    Task DeleteAsync(int id);
+    Task<RecipeDto> EditAsync(RecipeDto recipeModel);
+    Task<List<RecipeDto>> GetAllAsync();
+    Task<RecipeModel> GetAsync(int id);
+    Task<RecipesResponse> GetByKeyword(string keyword, int currentPageNumber, int pageSize);
+    Task<List<RecipeDto>> GetRecentsAsync();
 }
