658916747486af677954e126e12940ec4f526a31
Tyler Barlow <barlowtyler96@gmail.com>
2023-09-05 19:42:44 -0400

    dont need this model

--- RecipesB2CBlazor/Models/FileUploadViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace RecipesB2CBlazor.Models;
-
-public class FileUploadViewModel
-{
-    public string? FileName { get; set; }
-    public string? FileStorageUrl { get; set; }
-    public string? ContentType { get; set; }
-}
