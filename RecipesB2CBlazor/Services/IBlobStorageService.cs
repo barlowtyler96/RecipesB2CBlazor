591729c66901dcba206d5b03aeb8ab3ad2d4d071
Tyler Barlow <barlowtyler96@gmail.com>
2023-09-05 19:44:23 -0400

    dont need delete method

--- RecipesB2CBlazor/Services/IBlobStorageService.cs
+++ RecipesB2CBlazor/Services/IBlobStorageService.cs
@@ -1,6 +1,5 @@
 ﻿namespace RecipesB2CBlazor.Services;
 public interface IBlobStorageService
 {
-    Task<bool> DeleteFileToBlobAsync(string strFileName);
     Task<string> UploadFileToBlobAsync(string strFileName, string contecntType, Stream fileStream);
 }
