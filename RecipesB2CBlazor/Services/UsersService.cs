108a7b7604d894c94c137876f354973b2ef86aba
Tyler Barlow <barlowtyler96@gmail.com>
2025-01-03 17:03:14 -0500

    auth fixes

--- RecipesB2CBlazor/Services/UsersService.cs
+++ RecipesB2CBlazor/Services/UsersService.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.Identity.Web;
 using System.Net.Http.Headers;
+using System.Security.Claims;
 using Newtonsoft.Json;
 using RecipesB2CBlazor.Models;
 using System.Net;
+using Microsoft.AspNetCore.Components.Authorization;
 
 namespace RecipesB2CBlazor.Services;
 
@@ -19,11 +21,13 @@ public class UsersService : IUsersService
     private readonly string _usersScope = string.Empty;
     private readonly string _usersBaseAddress = string.Empty;
     private readonly ITokenAcquisition _tokenAcquisition;
+    private readonly AuthenticationStateProvider _authenticationStateProvider;
 
     public UsersService(ITokenAcquisition tokenAcquisition, HttpClient httpClient,
-        IConfiguration configuration)
+        IConfiguration configuration, AuthenticationStateProvider authenticationStateProvider)
     {
         _httpClient = httpClient;
+        _authenticationStateProvider = authenticationStateProvider;
         _tokenAcquisition = tokenAcquisition;
         _usersScope = configuration["DownstreamApi:Scopes"];
         _usersBaseAddress = configuration["DownstreamApi:BaseUrl"];
@@ -75,11 +79,11 @@ public class UsersService : IUsersService
         throw new HttpRequestException($"Invalid status code in the HttpResponseMessage: {response.StatusCode}");
     }
 
-    public async Task<bool> AddUserFavoriteAsync(int recipeId)
+    public async Task<bool> AddUserFavoriteAsync(int id)
     {
         await PrepareAuthenticatedClientForUser();
 
-        var request = new HttpRequestMessage(HttpMethod.Post, $"{_usersBaseAddress}Users/favorite/{recipeId}");
+        var request = new HttpRequestMessage(HttpMethod.Post, $"{_usersBaseAddress}Users/favorite/{id}");
 
         var response = await _httpClient.SendAsync(request);
 
@@ -121,8 +125,26 @@ public class UsersService : IUsersService
 
     private async Task PrepareAuthenticatedClientForUser()
     {
+        if (await IsNewUserAsync())
+        {
+            await AddNewUserAsync();
+        }
         var accessToken = await _tokenAcquisition.GetAccessTokenForUserAsync(new[] { _usersScope });
         _httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
         _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+    }
+
+    private async Task<bool> IsNewUserAsync()
+    {
+        var authState = await _authenticationStateProvider.GetAuthenticationStateAsync();
+        var user = authState.User;
+
+        if (user.Identity?.IsAuthenticated ?? false)
+        {
+            var newUserClaim = user.Claims.FirstOrDefault(c => c.Type == "newUser")?.Value;
+            return bool.TryParse(newUserClaim, out var isNewUser) && isNewUser;
+        }
+
+        return false;
     }
 }
